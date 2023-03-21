34e7e1c357cf93df6bd70ce5c1871e76a242e973
bengin34 <becaglar3434@gmail.com>
2023-03-21 17:47:26 +0300

    v4

--- src/API.ts
+++ src/API.ts
@@ -1,1 +1,34 @@
-https://the-trivia-api.com/api/questions?categories=sport_and_leisure,general_knowledge&limit=10&difficulty=medium
+import { shuffleArray } from "./utils";
+
+// https://the-trivia-api.com/api/questions?categories=sport_and_leisure,general_knowledge&limit=10&difficulty=medium
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answer: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+};
 
-const QuestionCard = () => {
-  return (
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => {
+  return <div>
+    <p className="number" >
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{__html: question}} />
     <div>
-      Question Card
+      {answers.map((answer) => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}  > <span dangerouslySetInnerHTML={{__html: answer}} /> </button>
+        </div>
+      ))}
     </div>
-  )
-}
+  </div>;
+};
 
-export default QuestionCard
+export default QuestionCard;
